a8858e66cb2e930631c3fcf064299a526ed39165
semajson <semajson@ymail.com>
2020-11-07 18:54:45 +0000

    added lisbon as well to app

--- src/TempChart/TempChart.js
+++ src/TempChart/TempChart.js
@@ -2,31 +2,38 @@ import { Doughnut, Bar, Line } from "react-chartjs-2";
 import React, { useState, useEffect } from "react";
 
 export function TempChart() {
-  const [tempData, setTempData] = useState([]);
+  const [LondonData, setLondonData] = useState([]);
+  const [LisbonData, setLisbonData] = useState([]);
   console.log("hello james");
 
+  async function getTempForecast(lat, lon, setData) {
+    fetch(
+      "https://api.openweathermap.org/data/2.5/onecall?lat=" +
+        lat +
+        "&lon=" +
+        lon +
+        "&exclude=daily,minutely&appid=" +
+        process.env.REACT_APP_WEATHER_API_KEY
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        let newData = [];
+        for (var hour in data.hourly) {
+          // For Javascript dates, need time in miliseconds not seconds.
+          // Also, convert temp to kelvin.
+          newData.push({
+            x: new Date(data.hourly[hour].dt * 1000),
+            y: (data.hourly[hour].temp - 273.15).toFixed(2),
+          });
+        }
+        console.log("new data is:", newData);
+        setData(newData);
+      });
+  }
+
   useEffect(() => {
-    async function fetchTempData() {
-      fetch(
-        "https://api.openweathermap.org/data/2.5/onecall?lat=51.5074&lon=-0.1278&exclude=daily,minutely&appid=" +
-          process.env.REACT_APP_WEATHER_API_KEY
-      )
-        .then((response) => response.json())
-        .then((data) => {
-          let newData = [];
-          for (var hour in data.hourly) {
-            // For Javascript dates, need time in miliseconds not seconds.
-            // Also, convert temp to kelvin.
-            newData.push({
-              x: new Date(data.hourly[hour].dt * 1000),
-              y: (data.hourly[hour].temp - 273.15).toFixed(2),
-            });
-          }
-          console.log("new data is:", newData);
-          setTempData(newData);
-        });
-    }
-    fetchTempData();
+    getTempForecast(51.5074, -0.1278, setLondonData);
+    getTempForecast(38.7223, -9.1393, setLisbonData);
   }, []);
 
   return (
@@ -34,19 +41,17 @@ export function TempChart() {
       <Line
         data={{
           datasets: [
-            /*
-            {
-              label: "My First dataset",
-              backgroundColor: "rgb(255, 99, 132)",
-              borderColor: "rgb(255, 99, 132)",
-              data: tempData,
-            },
-            */
             {
               label: "London",
               //backgroundColor: "rgb(55, 99, 132)",
               borderColor: "rgb(255, 99, 132)",
-              data: tempData,
+              data: LondonData,
+            },
+            {
+              label: "Lisbon",
+              //backgroundColor: "rgb(55, 99, 132)",
+              borderColor: "rgb(230, 0, 200)",
+              data: LisbonData,
             },
           ],
         }}
@@ -54,8 +59,8 @@ export function TempChart() {
         height={25}
         options={{
           title: {
-            text: "Hourly temperature forecast",
-            display: "Hourly temperature forecast",
+            text: "Temperature forecast (hourly)",
+            display: "Temperature forecast (hourly)",
           },
 
           scales: {
